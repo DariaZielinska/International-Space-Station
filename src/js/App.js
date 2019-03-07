fb85d0b905caafe9b814827f64ba5925a177bafe
DariaZielinska <dari.zielinska@gmail.com>
2019-03-07 16:11:30 +0100

    calculations

--- src/js/App.js
+++ src/js/App.js
@@ -1,53 +1,67 @@
 import React from 'react'
 import ReactFullpage from '@fullpage/react-fullpage';
-
+import Timestamp from 'react-timestamp';
 
 let prev = {
     longitude: 0,
     latitude: 0,
-    timestamp: 0
+    timestamp: 0,
 };
 
 let curr = {
     longitude: 0,
     latitude: 0,
-    timestamp: 0
+    timestamp: 0,
 };
-
 
 let generalInfo = {
     prev: prev,
     current: curr,
+    distanceBetweenTwo: 0,
     overallDistance: 0,
+    speed: 0,
 
     setNext: function(position, timestamp){
-        this.prev = this.current;
-        this.current.longitude = position.longitude;
-        this.current.latitude = position.latitude;
-        this.current.timestamp = timestamp;
-        console.log(this.current);
+
+        //change current data to previous data
+        this.prev.longitude = this.current.longitude;
+        this.prev.latitude = this.current.latitude;
+        this.prev.timestamp = this.current.timestamp;
+
+        //do actualization of current data
+        curr.longitude = position.longitude;
+        curr.latitude = position.latitude;
+        curr.timestamp = timestamp;
+
+        if(this.prev.timestamp === 0 ){
+            return null;
+        }else{
+            this.calculateDistance();
+            this.calculateSpeed();
+        }
     },
 
-    getDistance: function() {
-        return 0;
+    calculateDistance: function() {
+        const degToRad = Math.PI / 180;
+        const R = 6371; // km Earth radius
+        let distance = R * degToRad * Math.sqrt(Math.pow(Math.cos(this.prev.latitude * degToRad ) * (this.prev.longitude - this.current.longitude) , 2) + Math.pow(this.prev.latitude - this.current.latitude, 2));
+        this.distanceBetweenTwo = distance;
+        this.overallDistance =  this.overallDistance + Math.floor(distance);
     },
 
-    getSpeed: function(){
-        return 0;
-    }
+    calculateSpeed: function(){
+        this.speed = Math.round((this.distanceBetweenTwo / (this.current.timestamp - this.prev.timestamp)) * 100) / 100;
+    },
 };
 
-function getIss2(){
-    return {
-        Isss: 0
-    }
-}
 class Section1 extends React.Component{
+
     render(){
         return ( <div>
-            <p>latitude: {this.props.generalInfo.current.latitude}</p>
-            <p>longitude: {this.props.generalInfo.current.longitude}</p>
-            </div>)
+                    <p>latitude: {this.props.generalInfo.current.latitude}</p>
+                    <p>longitude: {this.props.generalInfo.current.longitude}</p>
+                    <Timestamp time={this.props.generalInfo.current.timestamp} format='full'/>
+                </div>)
     }
 }
 
@@ -56,7 +70,8 @@ class Fullpage extends React.Component{
         super(props);
         this.state = {
             isError: false,  //rozwinac to jak bedzie blad
-            generalInfo: generalInfo,
+            all: generalInfo,
+            isOk: false,
         }
     }
 
@@ -66,20 +81,16 @@ class Fullpage extends React.Component{
             <ReactFullpage
                 render={({ state, fullpageApi }) => {
                     return (
-                        <ReactFullpage.Wrapper  >
+                        <ReactFullpage.Wrapper>
 
                                 <div className="section first">
-
                                     <i className="fas fa-user-astronaut"> </i>
                                     <h1>INTERNATIONAL SPACE STATION</h1>
-                                    <p>Humankind’s Remarkable Achievement and World-Class Laboratory. Where it is now? </p>
+                                    <p>Humankind’s Remarkable Achievement and World-Class Laboratory. <strong>Where it is now?</strong> </p>
                                     <button onClick={() => fullpageApi.moveSectionDown()}>
-
                                         <p>scroll down</p>
                                         <i className="fas fa-angle-double-down"> </i>
-
                                     </button>
-
                                 </div>
 
                                 <div className="section second">
@@ -87,13 +98,27 @@ class Fullpage extends React.Component{
                                     <div className="rocket">
                                         <i className="fas fa-space-shuttle"> </i>
                                     </div>
-                                    <Section1 generalInfo={this.state.generalInfo}/>
-                                    <p>TODO: get speed </p>
-                                    <p>TODO: get distance</p>
-                                    <button onClick={this.getISS}> REFRESH </button>
+                                    <main>
+                                        <h2>IT IS NOW ON:</h2>
+                                        <div className="details">
+                                            <div>
+                                                <p>{generalInfo.current.latitude}</p>
+                                                <p>latitude </p>
+                                            </div>
+                                            <div>
+                                                <p>{generalInfo.current.longitude}</p>
+                                                <p>longitude </p>
+                                            </div>
+                                        </div>
+                                        <p className="time">(<Timestamp time={generalInfo.current.timestamp} format='full'/>)</p>
+                                        <button onClick={this.getISS}> SHOW ME MORE DETAILS </button>
+                                        <div className="more">
+                                            <p>It's {this.state.all.overallDistance}km travelled by ISS since you're here!</p>
+                                            <p>That's speed is <strong>{this.state.all.speed}km/s </strong> now, when average speed = 7,66km/s </p>
+                                        </div>
+                                    </main>
 
-                                    <button onClick={this.getDistance}>speed</button>
-                                    <button onClick={this.getSpeed}>distance</button>
+
                                 </div>
 
                         </ReactFullpage.Wrapper>
@@ -108,7 +133,8 @@ class Fullpage extends React.Component{
     };
 
     getISS = () => {
-        fetch('http://api.open-notify.org/iss-now.json')
+        fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json')
+        // old: http://open-notify.org/Open-Notify-API/ISS-Location-Now/ but it's http (danger)
             .then(issPosition => {
                 if (issPosition.ok){
                     return issPosition.json()
@@ -119,32 +145,13 @@ class Fullpage extends React.Component{
                 }
             })
             .then(issPositionJSON => {
-
                 generalInfo.setNext(issPositionJSON.iss_position, issPositionJSON.timestamp);
-
                 this.setState({
-                    generalInfo: generalInfo,
+                    isOk: true
                 });
             })
             .catch(()=>{this.setState({isError: true})})
     };
-
-
-SPEED            //     points = [{lat: .., lng: ..}, ... ]; // 6 points
-                // distancesSum = 0;
-                // for(i = 0; i < distances.length - 1; i++) {
-                //     distancesSum += distance(points[i], points[i + 1]);
-                // }
-                // return (distancesSum / (points.length - 1));
-
-    // function distance(point1, point2) {
-    //     var degToRad = Math.PI / 180;
-    //     return R * degToRad * Math.sqrt(Math.pow(Math.cos(point1.lat * degToRad ) * (point1.lng - point2.lng) , 2) + Math.pow(point1.lat - point2.lat, 2));
-    // }
-
-    // R = 6371000;// meters Earth radius
-
-
 }
 
 class App extends React.Component {
