2ebcceb3c93415900d030d50bbea4be860b4a2ab
DariaZielinska <dari.zielinska@gmail.com>
2019-03-08 21:39:19 +0100

    bugfix: too fast click caused incorrect distance calculations

--- src/js/App.js
+++ src/js/App.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import ReactFullpage from '@fullpage/react-fullpage';
 import Timestamp from 'react-timestamp';
-
-let prev = {
-    longitude: 0,
-    latitude: 0,
-    timestamp: 0,
-};
-
-let curr = {
-    longitude: 0,
-    latitude: 0,
-    timestamp: 0,
-};
+import CountUp from 'react-countup';
 
 let generalInfo = {
-    prev: prev,
-    current: curr,
+    prev: {
+        longitude: 0,
+        latitude: 0,
+        timestamp: 0,
+    },
+    current: {
+        longitude: 0,
+        latitude: 0,
+        timestamp: 0,
+    },
     distanceBetweenTwo: 0,
     overallDistance: 0,
     speed: 0,
 
     setNext: function(position, timestamp){
+
+        if(this.current.timestamp === timestamp){
+            return;
+        }
 
         //here: changing current data to previous
         this.prev.longitude = this.current.longitude;
@@ -29,13 +30,11 @@ let generalInfo = {
         this.prev.timestamp = this.current.timestamp;
 
         //do actualisation of current data
-        curr.longitude = position.longitude;
-        curr.latitude = position.latitude;
-        curr.timestamp = timestamp;
+        this.current.longitude = position.longitude;
+        this.current.latitude = position.latitude;
+        this.current.timestamp = timestamp;
 
-        if(this.prev.timestamp === 0 ){
-            return null;
-        }else{
+        if(this.prev.timestamp !== 0 ){
             this.calculateDistance();
             this.calculateSpeed();
         }
@@ -50,7 +49,7 @@ let generalInfo = {
     },
 
     calculateSpeed: function(){
-        this.speed = Math.round((this.distanceBetweenTwo / (this.current.timestamp - this.prev.timestamp)) * 100) / 100;
+        this.speed = (this.distanceBetweenTwo / (this.current.timestamp - this.prev.timestamp)).toFixed(2);
     },
 };
 
@@ -67,22 +66,19 @@ class Fullpage extends React.Component{
     }
 
     render(){
-
-        // let state = this.state.isDetailsVisible?  "blackButton" : "whiteButton";
-
         let details = null;
+
+        const previousDistance = this.state.all.overallDistance - this.state.all.distanceBetweenTwo;
+        const currentDistance = this.state.all.overallDistance;
 
         if (this.state.isDetailsVisible === true) {
             details = (
                 <div className="details">
-                    <p>It's <strong>{this.state.all.overallDistance}km</strong> travelled by ISS since you're here!</p>
+                    <p>It's <strong><CountUp start={previousDistance} end={currentDistance}/>km</strong> travelled by ISS since you're here!</p>
                     <p>Based on these information, the calculated speed of ISS is <strong>{this.state.all.speed}km/s </strong> now, when average speed = 7,66km/s </p>
                 </div>
             )
         }
-
-
-
 
         return <div className="background-sky">
             <ReactFullpage
@@ -90,38 +86,38 @@ class Fullpage extends React.Component{
                     return (
                         <ReactFullpage.Wrapper>
 
-                                <div className="section first">
-                                    <i className="fas fa-user-astronaut"> </i>
-                                    <h1>INTERNATIONAL SPACE STATION</h1>
-                                    <p>Humankind’s Remarkable Achievement and World-Class Laboratory. <strong>Where it is now?</strong> </p>
-                                    <button onClick={() => fullpageApi.moveSectionDown()}>
-                                        <p>scroll down</p>
-                                        <i className="fas fa-angle-double-down"> </i>
-                                    </button>
+                            <div className="section first">
+                                <i className="fas fa-user-astronaut"> </i>
+                                <h1>INTERNATIONAL SPACE STATION</h1>
+                                <p>Humankind’s Remarkable Achievement and World-Class Laboratory. <strong>Where it is now?</strong> </p>
+                                <button onClick={() => fullpageApi.moveSectionDown()}>
+                                    <p>scroll down</p>
+                                    <i className="fas fa-angle-double-down"> </i>
+                                </button>
+                            </div>
+
+                            <div className="section second">
+
+                                <div className="rocket">
+                                    <i className="fas fa-space-shuttle"> </i>
                                 </div>
-
-                                <div className="section second">
-
-                                    <div className="rocket">
-                                        <i className="fas fa-space-shuttle"> </i>
-                                    </div>
-                                    <main>
-                                        <h2>IT IS NOW ON:</h2>
-                                        <div className="localisation">
-                                            <div>
-                                                <p>{this.state.all.current.latitude}</p>
-                                                <p>latitude </p>
-                                            </div>
-                                            <div>
-                                                <p>{this.state.all.current.longitude}</p>
-                                                <p>longitude </p>
-                                            </div>
+                                <main>
+                                    <h2>IT IS NOW ON:</h2>
+                                    <div className="localisation">
+                                        <div>
+                                            <p>{this.state.all.current.latitude}</p>
+                                            <p>latitude </p>
                                         </div>
-                                        <p className="time">(<Timestamp time={this.state.all.current.timestamp} format='full'/>)</p>
-                                        <button onClick={this.showDetails.bind(this)}> {this.state.buttonText} </button>
-                                        {details}
-                                    </main>
-                                </div>
+                                        <div>
+                                            <p>{this.state.all.current.longitude}</p>
+                                            <p>longitude </p>
+                                        </div>
+                                    </div>
+                                    <p className="time">(<Timestamp time={this.state.all.current.timestamp} format='full'/>)</p>
+                                    <button onClick={this.showDetails} disabled={!this.state.isOk}> {this.state.buttonText} </button>
+                                    {details}
+                                </main>
+                            </div>
 
                         </ReactFullpage.Wrapper>
                     );
@@ -146,6 +142,7 @@ class Fullpage extends React.Component{
 
     getISS = () => {
         //TODO delete this and write async func
+        //TODO loaders while getting data
         fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json')
             .then(issPosition => {
                 if (issPosition.ok){
@@ -163,7 +160,7 @@ class Fullpage extends React.Component{
                 });
             })
             .catch(()=>{
-                this.setState({isError: true})
+                this.setState({isError: true});
             })
     };
 }
